5fd57505015c18f22b4a6a07020a30536f43bfaf
David Hilera <david.hilera@hotmail.com>
2025-07-04 18:50:17 -0600

    feat: update promotions and enhance promotion banner layout

--- src/components/promotion-banner.tsx
+++ src/components/promotion-banner.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { ShoppingCart, Tag } from 'lucide-react'
+import { ShoppingCart } from 'lucide-react'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
@@ -54,15 +54,10 @@ export function PromotionBanner({ promotions }: PromotionBannerProps) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center max-w-screen-lg mx-auto">
       {activePromotions.map((promotion, index) => {
-        // Calculate total price and discounted price
-        const { originalPrice, promoPrice } = promotion
-        const discountAmount = originalPrice - promoPrice
-        const discountPercentage = promotion.discountPercentage
-
         return (
           <motion.div
             key={promotion.id}
-            className="bg-card border border-primary rounded-lg shadow-md overflow-hidden max-w-lg h-full flex flex-col"
+            className="bg-card border border-primary rounded-lg shadow-md overflow-hidden max-w-lg w-full h-full flex flex-col"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: index * 0.1 }}
@@ -80,32 +75,33 @@ export function PromotionBanner({ promotions }: PromotionBannerProps) {
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent flex flex-col justify-end p-4">
                 <h3 className="font-bold text-xl text-white">{promotion.name}</h3>
-                <div className="flex items-center mt-1">
+                {/* <div className="flex items-center mt-1">
                   <Tag className="h-4 w-4 text-white mr-1" />
                   <span className="text-white font-semibold text-sm">
                     {discountPercentage}% DESCUENTO
                   </span>
-                </div>
+                </div> */}
               </div>
             </div>
             <div className="p-4 flex-grow justify-between flex flex-col">
               <p className="text-muted-foreground mb-3">{promotion.description}</p>
               <div className="flex justify-between items-center mb-4">
-                <div>
+                {/* <div>
                   <span className="text-lg font-bold text-destructive">${promoPrice.toFixed(2)}</span>
                   <span className="text-sm text-muted-foreground line-through ml-2">${originalPrice.toFixed(2)}</span>
-                </div>
-                <span className="bg-destructive/20 text-red-800 text-xs font-semibold px-2 py-1 rounded">
+                </div> */}
+                {/* <span className="bg-destructive/20 text-red-800 text-xs font-semibold px-2 py-1 rounded">
                   AHORRA ${discountAmount.toFixed(2)}
-                </span>
+                </span> */}
               </div>
             </div>
 
             <div className="flex justify-between items-center p-4 pt-0">
               <Button
+                variant='secondary'
                 onClick={() => { handleAddPromoToCart(promotion) }}
                 disabled={isLoading}
-                className="w-full bg-primary hover:bg-primary/80 transition-colors"
+                className="w-full"
               >
                 <ShoppingCart className="mr-2 h-4 w-4" />
                 Agregar al carrito
